ed1998fba6ac4d623942809911cdf4898212c5ba
Samson-Dai <dscsamson@gmail.com>
2022-04-07 17:04:40 -0400

    Init

--- pipeline.sh
+++ pipeline.sh
@@ -8,17 +8,23 @@ module load anaconda3/2020.11
 mv $HOME/.conda $PROJECT/.conda
 ln -s $PROJECT/.conda $HOME/.conda
 
-#download package via anaconda
-conda install -c bioconda -c conda-forge umi_tools
-conda install -c bioconda fastq-tools
-
 #download package from github
-#https://github.com/YeoLab/clipper
+##clipper
 git clone https://github.com/YeoLab/clipper.git
 cd clipper
 conda env create -f environment3.yml
 conda activate clipper3
 pip install .
+##umi_tools
+git clone https://github.com/CGATOxford/UMI-tools.git
+cd cd UMI-tools
+python setup.py install
+##fastq_tools
+git clone https://github.com/dcjones/fastq-tools.git
+bash autogen.sh
+./configure
+make install
+ln -s /usr/bin /jet/home/sdai/bin
 
 
 #trim adapters
