5dc4065e72a94c2c2b2547a73abb4e25cea87646
TianyuXia123 <90228477+TianyuXia123@users.noreply.github.com>
2022-04-19 11:57:57 -0400

    Update RCAS.R

--- r_scripts/RCAS.R
+++ r_scripts/RCAS.R
@@ -6,8 +6,8 @@ library(RCAS)
 
 queryRegions <- importBed(filePath = "initial_peaks.bed", sampleN = 10000)
 
-# Annotationfile: hg38.gtf (downloaded from UCSC Genome Browser)
-gff <- importGtf(filePath = "hg38.gtf")
+# Annotationfile: hg38_UCSC.gtf (downloaded from UCSC Genome Browser)
+gff <- importGtf(filePath = "hg38_UCSC.gtf")
 
 overlaps <- as.data.table(queryGff(queryRegions = queryRegions, gffData = gff))
 
@@ -24,8 +24,8 @@ fig = ggplot2::ggplot(df, aes(x = reorder(feature, -percent), y = percent)) +
   theme(axis.text.x = element_text(angle = 90))
 ggsave(fig,filename = "RCAS_result_UCSC.png",width = 10,height = 8)
 
-# Annotation file: Homo_sapiens.GRCh38.106.gtf (downloaded from ENSEMBLE database)
-gff <- importGtf(filePath = "Homo_sapiens.GRCh38.106.gtf")
+# Annotation file: hg38_ENSEMBL.gtf (downloaded from ENSEMBLE database)
+gff <- importGtf(filePath = "hg38_ENSEMBL.gtf")
 
 overlaps <- as.data.table(queryGff(queryRegions = queryRegions, gffData = gff))
 
@@ -40,4 +40,4 @@ fig = ggplot2::ggplot(df, aes(x = reorder(feature, -percent), y = percent)) +
   labs(x = 'transcript feature', y = paste0('percent overlap (n = ', length(queryRegions), ')')) + 
   theme_bw(base_size = 14) + 
   theme(axis.text.x = element_text(angle = 90))
-ggsave(fig,filename = "RCAS_result_ENSEMBLE.png",width = 10,height = 8)
+ggsave(fig,filename = "RCAS_result_ENSEMBL.png",width = 10,height = 8)
