b160ea3895c8e050d3a42e79d03e6708b990ceb5
Samson-Dai <dscsamson@gmail.com>
2022-05-02 00:03:53 -0400

    Update slurm memory

--- sample_submission.sh
+++ sample_submission.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #SBATCH -p RM-shared
 #SBATCH -t 5:00:00
+#SBATCH --mem=40G
 
 ./eclippe -i /jet/home/sdai/clip_proj/input_data -o /jet/home/sdai/clip_proj/output_dir -t /jet/home/sdai/clip_proj/temp_dir
