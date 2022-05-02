a9e7da7abcde7b374c5248e1dd4700c70d089569
Samson-Dai <dscsamson@gmail.com>
2022-05-01 23:36:21 -0400

    Update nodes

--- sample_submission.sh
+++ sample_submission.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 #SBATCH -p RM-shared
 #SBATCH -t 5:00:00
-#SBATCH -N 2
-#SBATCH -n 2
 
 ./eclippe -i /jet/home/sdai/clip_proj/input_data -o /jet/home/sdai/clip_proj/output_dir -t /jet/home/sdai/clip_proj/temp_dir
