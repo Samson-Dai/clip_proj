f1a6a1ef8f4ccf577e3a83a348b65e6051133e50
Samson-Dai <dscsamson@gmail.com>
2022-05-01 23:05:28 -0400

    Update submission script

--- sample_submission.sh
+++ sample_submission.sh
@@ -2,4 +2,4 @@
 #SBATCH -p RM-shared
 #SBATCH -t 5:00:00
 
-./eclippe -i /Users/Administrator/Desktop/samson1/CMU/year2_sem2/03-713_Bioinformatics_Data_Integration_Practicum/project/input_sample -o /Users/Administrator/Desktop/samson1/CMU/year2_sem2/03-713_Bioinformatics_Data_Integration_Practicum/project/output_sample -t /Users/Administrator/Desktop/samson1/CMU/year2_sem2/03-713_Bioinformatics_Data_Integration_Practicum/project/temp_sample
+./eclippe -i /jet/home/sdai/clip_proj/input_data -o /jet/home/sdai/clip_proj/output_dir -t /jet/home/sdai/clip_proj/temp_dir
